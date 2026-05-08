94840a0960046d0753a9e750e2ae2390db7e71b0
jaisri <jaisriarangasamy@gmail.com>
2026-05-08 22:03:10 +0530

    removed slash from vercel url

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://matrimony-client-psi.vercel.app/",
+      "https://matrimony-client-psi.vercel.app",
     ],
     credentials: true,
   }),
