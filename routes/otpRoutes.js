1ddd3917211dbd2e14f7333bb3a89521fe735ae3
jaisri <jaisriarangasamy@gmail.com>
2026-05-14 10:13:42 +0530

    removec protect from otp

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { sendOTP, verifyOTP } from "../controllers/otpController.js";
-import { protect } from "../middleware/authMiddleware.js";
+// import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // ✅ MUST be protected (user already logged in)
-router.post("/send", protect, sendOTP);
-router.post("/verify", protect, verifyOTP);
+router.post("/send", sendOTP);
+router.post("/verify", verifyOTP);
 
 export default router;
